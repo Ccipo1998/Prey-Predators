b756d59333e4b14ef927e0943ea0323b9ddbc78f
Andrea PC-Desktop <andrea.cipollini98@gmail.com>
2022-06-04 22:29:36 +0200

    Basic wandering added

--- Assets/Actions/ZebraSearch.cs
+++ Assets/Actions/ZebraSearch.cs
@@ -108,7 +108,9 @@ namespace Assets.Actions
                     else
                     {
                         // else wandering
-                        CurrentNavMeshAgent.destination = new Vector3(10.0f, 0.0f, 0.0f);
+                        Wander();
+
+                        //CurrentNavMeshAgent.destination = new Vector3(10.0f, 0.0f, 0.0f);
                     }
 
                     break;
@@ -179,87 +181,14 @@ namespace Assets.Actions
 
                     break;
             }
-            /*
-            // check if the animal can request a spot
-            if (IsNearFreeSpot())
-            {
-                // if the animal is not in queue for a free spot -> add to queue
-                if (!InQueue)
-                {
-                    CurrentNearerFreeSpot.ToQueue(gameObject);
-                    InQueue = true;
-                }
-                // if the animal is already in queue for a free spot -> request spot assignment
-                else if (Status == SearchingStatus.Searching)
-                {
-                    GameObject assigned = CurrentNearerFreeSpot.AssignSpot();
-                    if (assigned == gameObject)
-                    {
-                        // clear queue and occupy the spot
-                        CurrentNearerFreeSpot.ClearQueue();
-                        CurrentNearerFreeSpot.Occupy();
-                        Status = SearchingStatus.Arrived;
+        }
 
-                        // set position and look at
-                        transform.position = CurrentNearerFreeSpot.Position;
-                        gameObject.transform.LookAt(CurrentNearerFreeResource.transform.position, gameObject.transform.up);
-                    }
-                }
-            }
-            else
-            {
-                // reset status
-                Status = SearchingStatus.Searching;
+        public void Wander()
+        {
+            float maxRotation = 120.0f;
+            Vector3 randomDirection = Quaternion.AngleAxis(Random.Range(-maxRotation, maxRotation), gameObject.transform.up) * gameObject.transform.forward;
 
-                // check resource and spot in FOV
-                GameObject nearerFreeResource;
-                if (searchType == SearchType.Food)
-                    nearerFreeResource = CurrentZebraFOV.GetNearerFreeFood();
-                else
-                    nearerFreeResource = CurrentZebraFOV.GetNearerFreeWater();
-
-                // free spot found
-                if (nearerFreeResource != null)
-                {
-                    ResourceSpot nearerFreeSpot = nearerFreeResource.GetComponent<Resource>().GetNearerFreeSpot(CurrentPosition);
-                    // check if the spot changed
-                    if (nearerFreeSpot != CurrentNearerFreeSpot)
-                    {
-                        CurrentNearerFreeResource = nearerFreeResource;
-                        CurrentNearerFreeSpot = nearerFreeSpot;
-                        CurrentNavMeshAgent.destination = CurrentNearerFreeSpot.Position;
-                        CurrentNavMeshAgent.isStopped = false;
-                    }
-                    else
-                    {
-                        CurrentNavMeshAgent.destination = CurrentNearerFreeSpot.Position;
-                        CurrentNavMeshAgent.isStopped = false;
-                    }
-                }
-                // else search basing on knowledge
-                else if (searchType == SearchType.Food && CurrentKnowledge.LastFoundedFood != null)
-                {
-                    // clear current resource/spot
-                    CurrentNearerFreeResource = null;
-                    CurrentNearerFreeSpot = null;
-
-                    CurrentNavMeshAgent.destination = CurrentKnowledge.LastFoundedFood.position;
-                }
-                else if (searchType == SearchType.Water && CurrentKnowledge.LastFoundedWater != null)
-                {
-                    // clear current resource/spot
-                    CurrentNearerFreeResource = null;
-                    CurrentNearerFreeSpot = null;
-
-                    CurrentNavMeshAgent.destination = CurrentKnowledge.LastFoundedWater.position;
-                }
-                // else wandering
-                else
-                {
-                    CurrentNavMeshAgent.destination = new Vector3(10.0f, 0.0f, 0.0f);
-                }
-            }
-            */
+            CurrentNavMeshAgent.destination = gameObject.transform.position + randomDirection * 2;
         }
 
         private void OnDisable()
